8c86d6b36acd0fb1db1bbe54c909a41fabbd6663
ML6503 <ml6503@gmail.com>
2021-03-21 04:10:30 +0200

    fix: keys are now active until keyup event

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -8,6 +8,7 @@ const buttonsContainer = document.querySelector('.btn-container');
 
 // we declare active classes constants
 const PIANO_ACTIVE_CLASS = 'piano-key-active';
+const PIANO_ACTIVE_PSEUDO_CLASS = 'piano-key-active-pseudo';
 const BTN_ACTIVE_CLASS = 'btn-active';
 
 // we declare the button that can change notes to letters 
@@ -18,9 +19,9 @@ function addActiveClass(target, activeClass) {
     const array = activeClass === PIANO_ACTIVE_CLASS ? pianoKeys : activeClass === BTN_ACTIVE_CLASS ? buttons : [];
 
     if (activeClass === BTN_ACTIVE_CLASS) {
-        pianoKeys.forEach(k => {            
+        pianoKeys.forEach(k => {
             if (k.dataset.letter !== undefined && btnChange) {
-                k.classList.toggle('piano-key-letter');               
+                k.classList.toggle('piano-key-letter');
             }
         });
     }
@@ -31,6 +32,10 @@ function addActiveClass(target, activeClass) {
         }
     });
     target.classList.add(activeClass);
+    if (activeClass === PIANO_ACTIVE_CLASS) {
+        target.classList.add(PIANO_ACTIVE_PSEUDO_CLASS);
+    }
+
 };
 
 // play note sound
@@ -46,15 +51,23 @@ function playAudio(target) {
 };
 
 // play note on mouse click
-piano.addEventListener('click', e => playAudio(e.target));
+piano.addEventListener('mousedown', e => playAudio(e.target));
 
 // we remove class 'piano-key-active' from inactive key on 'transitionend' event
 function removeActiveKey(e) {
+
     if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('piano-key-active');
+    e.target.classList.remove(PIANO_ACTIVE_CLASS);
+    e.target.classList.remove(PIANO_ACTIVE_PSEUDO_CLASS);
+    isUp = false;
 };
 
-pianoKeys.forEach(key => key.addEventListener('transitionend', removeActiveKey));
+let isUp = false;
+window.addEventListener('keydown', () => isUp = true );
+if(isUp) {
+    pianoKeys.forEach(key => key.addEventListener('transitionend', removeActiveKey));
+}
+
 
 // play note on mousedown + mousemove
 let isPressed = false;
@@ -74,24 +87,44 @@ window.addEventListener('mouseup', e => {
 
 // play notes from keyboard
 window.addEventListener('keydown', e => {
+    const repeat = e.repeat;
+
     const letter = e.code.slice(3);
 
     if (letter !== undefined && letter.length === 1) {
         const noteTarget = [...pianoKeys].filter(k => k.dataset.letter === letter)[0];
-        // console.log('chosen div key from keydown',  [...pianoKeys].filter(k => k.dataset.letter === letter)[0] );
 
-        playAudio(noteTarget);
+        if (!repeat) {
+            playAudio(noteTarget);
+        }
     }
 });
 
 /* changing notes to letters and active class of Notes / Letters buttons */
 
-
 //on click buttons toggling active class and letters-notes
-function toggleButton(e) {    
+function toggleButton(e) {
     btnChange = e.target.classList.contains('btn-active') ? false : true;
-    
+
     addActiveClass(e.target, BTN_ACTIVE_CLASS)
 };
 
 buttonsContainer.addEventListener('click', e => toggleButton(e));
+
+/* full screen */
+
+const screenButton = document.querySelector('.fullscreen');
+
+function toggleFullscreen() {
+    let elem = document.querySelector('body');
+
+    if (!document.fullscreenElement) {
+        elem.requestFullscreen().catch(err => {
+            alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+        });
+    } else {
+        document.exitFullscreen();
+    }
+};
+
+screenButton.addEventListener('click', () => toggleFullscreen())
