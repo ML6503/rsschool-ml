1c33e7d5fe658e081762135e49e1ac833a8000ee
ML6503 <ml6503@gmail.com>
2021-04-26 23:08:15 +0300

    feat: added dark mode vars for landing

--- online-zoo/pages/index/indexJS.js
+++ online-zoo/pages/index/indexJS.js
@@ -3,3 +3,13 @@
 //   transform: scale(1.3);
 // }
 
+// function toggleDark() {
+//   const body = document.getElementsByClassName("background-body");
+//   body.classList.toggle("dark-mode");
+//   const zooPara = document.getElementsByClassName("zoo-online-article-p");
+//   body.classList.toggle("p-dark");
+// }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.documentElement.setAttribute("data-theme", "dark");
+});
